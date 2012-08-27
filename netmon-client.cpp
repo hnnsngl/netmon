2289ff2bd5209d663d6c54141fa43082309c9c70
Hannes Nagel <hannes.nagel@itp.uni-leipzig.de>
2012-08-28 00:31:19 +0200

    add some primitive structure to client

--- netmon-client.cpp
+++ netmon-client.cpp
@@ -1,12 +1,31 @@
 #include <iostream>
+#include <fstream>
 
 #include "netgroup.hpp"
 
 int main( int argc, char** argv )
 {
-	// retrieve default host list for netmon
-	std::vector<std::string> netmonHosts;
-	for( const auto triple : getNetGroup( "workstation-all" ) )
-		netmonHosts.push_back( std::get<0>(triple) );
+	// look for user host list file ~/.netmon-hosts
+	std::ifstream if_netmonhosts(".netmon-hosts");
+	if(if_netmonhosts) {
+
+	}
+	else {
+		// retrieve default host list for netmon
+		std::vector<std::string> netmonHosts;
+		for( const auto triple : getNetGroup( "workstation-all" ) )
+			netmonHosts.push_back( std::get<0>(triple) );
+	}	
+
+	// connect to hosts
 	
+
+	// read data
+
+
+	// parse data
+
+
+	// show data
+
 }
