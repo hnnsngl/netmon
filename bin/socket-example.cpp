3985130c34833a5985b10941f4e9c69fad82b5c0
Martin Marenz <martin.marenz@googlemail.com>
2012-08-29 17:58:05 +0200

    example can treat arbitrary number of agents asynchron

--- bin/socket-example.cpp
+++ bin/socket-example.cpp
@@ -1,6 +1,9 @@
 #include "../src/socket.h"
 #include <string>
 #include <iostream>
+#include <future>
+#include <functional>
+#include <vector>
 
 std::string recv_full_message( const std::string& host, const std::string& port ){
   Socket sock;
@@ -18,10 +21,19 @@ std::string recv_full_message( const std::string& host, const std::string& port 
 
 int main ( int argc, char *argv[] )
 {
-  if( argc != 3 ){
-    std::cout << "Usage: " << *argv << " adress port\n";
+  if( argc < 2 ){
+    std::cout << "Usage: " << *argv << " port adress1 adress2 ...\n";
     exit(1);
   }
-  std::cout << recv_full_message( argv[1], argv[2] );
+  int number_adresses = argc - 1;
+  std::vector< std::future<std::string> > tasks;
+  for(int i = 0; i < number_adresses; ++i){
+    tasks.push_back( async( std::bind( recv_full_message, argv[2], argv[1]) ) );
+  }
+  for(int i = 0; i < number_adresses; ++i){
+    std::cout << tasks[i].get();
+    std::cout << "##############################################\n";
+
+  }
   return EXIT_SUCCESS;
 }				/* ----------  end of function main  ---------- */
